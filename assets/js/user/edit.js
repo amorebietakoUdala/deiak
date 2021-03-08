8a82cfb369e4baf3726f06e322d12bbd6ed6e9d0
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2021-03-08 08:50:46 +0100

    Added user administratation templates and menu not showing if not logged in

--- assets/js/user/edit.js
+++ assets/js/user/edit.js
@@ -2,9 +2,13 @@ import '../../css/user/edit.scss';
 
 import $ from 'jquery';
 
-$(document).ready(function(){
-    $('.js-save').on('click',function (e) {
-        console.log('saveButtonClicked!');
+$(document).ready(function() {
+    $('.js-save').on('click', function(e) {
+        $(document.user).attr('action', $(e.currentTarget).data("url"));
+        document.user.submit();
+    });
+
+    $('.js-back').on('click', function(e) {
         $(document.user).attr('action', $(e.currentTarget).data("url"));
         document.user.submit();
     });
